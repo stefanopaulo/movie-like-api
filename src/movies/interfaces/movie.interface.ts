f7e01a6e91e68db0f563a844d90d068f1f9703a0
spsouza <stefanopaulo47@gmail.com>
2023-08-30 21:28:00 -0300

    Integrando API de curtidas com o frontend e ajustando o cors

--- src/movies/interfaces/movie.interface.ts
+++ src/movies/interfaces/movie.interface.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 export interface Movie extends mongoose.Document {
+  _id: string;
   title: string;
   popularity: number;
   likes: number;
--- src/movies/schemas/movie.schema.ts
+++ src/movies/schemas/movie.schema.ts
@@ -2,6 +2,7 @@
 import * as mongoose from 'mongoose';
 
 export const MovieSchema = new mongoose.Schema({
+  _id: String,
   title: String,
   popularity: Number,
   likes: Number,
