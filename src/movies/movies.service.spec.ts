162671664363cdc192e920365df1c49dcd6d0722
spsouza <stefanopaulo47@gmail.com>
2023-09-01 13:30:39 -0300

    Ajustando testes

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,90 +1,108 @@
+/* eslint-disable prettier/prettier */
 import { Movie } from './interfaces/movie.interface';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { MovieSchema } from './schemas/movie.schema';
 import { NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
 
-describe('MovieService', () => {
-  let movieService: MoviesService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        MongooseModule.forRoot('mongodb://localhost:27017/test', {}),
-        MongooseModule.forFeature([{ name: 'Movie', schema: MovieSchema }]),
-      ],
-      providers: [MoviesService],
-    }).compile();
-
-    movieService = module.get<MoviesService>(MoviesService);
-  });
-
-  it('should be defined', () => {
-    expect(movieService).toBeDefined();
-  });
-
-  it('should increment likes for an existing movie', async () => {
-    // Create a movie in the test database
-    const movieId = 'testMovieId';
-    const initialMovie = new Movie({ _id: movieId, likes: 1 });
-    await initialMovie.save();
-
-    // Call the likeMovie method
-    const updatedMovie = await movieService.likeMovie(movieId);
-
-    // Check if the likes have been incremented
-    expect(updatedMovie.likes).toBe(2);
-  });
-
-  it('should create a new movie with 1 like if the movie does not exist', async () => {
-    // Call the likeMovie method with a non-existing movieId
-    const movieId = 'nonExistingMovieId';
-    const newMovie = await movieService.likeMovie(movieId);
-
-    // Check if a new movie with 1 like has been created
-    expect(newMovie._id).toBe(movieId);
-    expect(newMovie.likes).toBe(1);
-  });
-});
-
-describe('MovieService', () => {
-  let movieService: MoviesService;
+describe('MoviesService', () => {
+  let moviesService: MoviesService;
+  let movieModel: Model<Movie>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        MongooseModule.forRoot('mongodb://localhost:27017/test', {}),
-        MongooseModule.forFeature([{ name: 'Movie', schema: MovieSchema }]),
+      providers: [
+        MoviesService,
+        {
+          provide: getModelToken('Movie'), // Substitua 'Movie' pelo nome do seu modelo
+          useValue: {
+            findByIdAndUpdate: jest.fn(),
+            findById: jest.fn(),
+          },
+        },
       ],
-      providers: [MoviesService],
     }).compile();
 
-    movieService = module.get<MoviesService>(MoviesService);
+    moviesService = module.get<MoviesService>(MoviesService);
+    movieModel = module.get<Model<Movie>>(getModelToken('Movie'));
   });
 
-  it('should be defined', () => {
-    expect(movieService).toBeDefined();
+  describe('likeMovie', () => {
+    it('should add likes to an existing movie', async () => {
+      const existingMovie = new Movie({ _id: 'movieId', likes: 5 });
+      movieModel.findByIdAndUpdate.mockResolvedValueOnce(existingMovie);
+
+      const result = await moviesService.likeMovie('movieId');
+
+      expect(result.likes).toBe(6);
+    });
+
+    it('should create a new movie with likes when the movie does not exist', async () => {
+      const nonExistingMovieId = 'nonExistingMovieId';
+      movieModel.findByIdAndUpdate.mockResolvedValueOnce(null);
+      movieModel.create.mockResolvedValueOnce({ _id: nonExistingMovieId, likes: 1 });
+
+      const result = await moviesService.likeMovie(nonExistingMovieId);
+
+      expect(result._id).toBe(nonExistingMovieId);
+      expect(result.likes).toBe(1);
+    });
+
+    it('should throw NotFoundException when the movie does not exist and cannot be created', async () => {
+      const nonExistingMovieId = 'nonExistingMovieId';
+      movieModel.findByIdAndUpdate.mockResolvedValueOnce(null);
+      movieModel.create.mockResolvedValueOnce(null);
+
+      try {
+        await moviesService.likeMovie(nonExistingMovieId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toBe('Filme não encontrado!');
+      }
+    });
   });
 
   describe('getMovie', () => {
-    it('should return a movie if it exists', async () => {
-      const movieId = 'testMovieId';
-      const movieTitle = 'Test Movie';
-      const existingMovie = new Movie({ _id: movieId, title: movieTitle });
-      await existingMovie.save();
+    it('should return an existing movie', async () => {
+      const existingMovie = new Movie({ _id: 'movieId', title: 'Movie Title' });
+      movieModel.findById.mockResolvedValueOnce(existingMovie);
 
-      const foundMovie = await movieService.getMovie(movieId);
+      const result = await moviesService.getMovie('movieId');
 
-      expect(foundMovie).toBeDefined();
-      expect(foundMovie.title).toBe(movieTitle);
+      expect(result).toEqual(existingMovie);
     });
 
-    it('should throw NotFoundException if the movie does not exist', async () => {
+    it('should throw NotFoundException when the movie does not exist', async () => {
       const nonExistingMovieId = 'nonExistingMovieId';
+      movieModel.findById.mockResolvedValueOnce(null);
 
       try {
-        await movieService.getMovie(nonExistingMovieId);
+        await moviesService.getMovie(nonExistingMovieId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toBe('Filme não encontrado!');
+      }
+    });
+  });
+
+  describe('getLikesForMovie', () => {
+    it('should return the number of likes for an existing movie', async () => {
+      const existingMovie = new Movie({ _id: 'movieId', likes: 10 });
+      movieModel.findById.mockResolvedValueOnce(existingMovie);
+
+      const result = await moviesService.getLikesForMovie('movieId');
+
+      expect(result).toBe(10);
+    });
+
+    it('should throw NotFoundException when the movie does not exist', async () => {
+      const nonExistingMovieId = 'nonExistingMovieId';
+      movieModel.findById.mockResolvedValueOnce(null);
+
+      try {
+        await moviesService.getLikesForMovie(nonExistingMovieId);
       } catch (error) {
         expect(error).toBeInstanceOf(NotFoundException);
         expect(error.message).toBe('Filme não encontrado!');
